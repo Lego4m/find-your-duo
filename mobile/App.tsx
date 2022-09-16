a15a6632a802be33f4e7d87a6bba1736e5642db1
Leonardo Maron <leo22.maron@hotmail.com>
2022-09-16 18:50:13 -0300

    Power up

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,10 +8,10 @@ import {
   Inter_900Black,
 } from '@expo-google-fonts/inter';
 
-import { Home } from './src/screens/Home';
 
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +28,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Home /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </Background>
   );
 }
